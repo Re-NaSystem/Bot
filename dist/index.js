322f7573b8cae9de7cb41b78f8fbdb8a38c7341a
RenaOfficial <y.games@outlook.jp>
2023-09-23 19:52:12 +0900

    distを削除

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-require('dotenv').config();
-const index_1 = require("./modules/index");
-exports.client = new index_1.ExtendedClient();
-console.clear();
-exports.client.start();
