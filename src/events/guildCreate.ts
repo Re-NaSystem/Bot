44e76bdf7ad1e75d1686a8c76d145f6b227f398d
RenaOfficial <y.games@outlook.jp>
2023-09-23 20:32:48 +0900

    招待ログを出すよう変更

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,3 +1,4 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, Colors, EmbedBuilder } from 'discord.js';
 import { client } from '..';
 import { Event } from '../modules/index';
 
@@ -5,4 +6,45 @@ export default new Event('guildCreate', async (guild) => {
   client.user?.setActivity({
     name: `/help | ${client.guilds.cache.size} servers`
   })
+
+  const channel = client.channels.cache.get(process.env.INVITE_LOG_CHANNEL)
+  if (!channel || channel?.type !== ChannelType.GuildText) return
+
+  const button = new ActionRowBuilder<ButtonBuilder>().addComponents(
+    new ButtonBuilder()
+      .setCustomId(`invite-${guild.id}`)
+      .setLabel("サーバー招待を出力")
+      .setStyle(ButtonStyle.Success)
+      .setEmoji("👤"),
+    new ButtonBuilder()
+      .setCustomId(`serverId-${guild.id}`)
+      .setLabel("サーバーIDを出力")
+      .setStyle(ButtonStyle.Success)
+      .setEmoji("📄")
+  )
+
+  const embed = new EmbedBuilder()
+    .setTitle("サーバーに追加されました")
+    .setFields([
+      {
+        name: "追加されたBot",
+        value: client.user?.tag as string,
+      },
+      {
+        name: "サーバー",
+        value: `${guild.name}(${guild.id})`,
+      },
+    ])
+    .setColor(Colors.Aqua)
+    .setFooter({
+      iconURL: client.getUserData().icon,
+      text: client.getUserData().footer,
+    })
+
+  await channel
+    .send({
+      embeds: [embed],
+      components: [button],
+    })
+    .catch((e) => console.log(e))
 });
--- src/types/enviroment.d.ts
+++ src/types/enviroment.d.ts
@@ -5,6 +5,7 @@ declare global {
       CLIENT_ID: string;
       CLIENT_SECRET: string;
       MONGODB_CONNECTION_STRING: string;
+      INVITE_LOG_CHANNEL: string;
     }
   }
 }
