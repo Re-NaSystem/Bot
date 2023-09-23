b529234e4e155037f4f9e279b71d04ee086f6a92
RenaOfficial <y.games@outlook.jp>
2023-09-23 17:46:41 +0900

    Pingコマンドを作成

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,9 +1,25 @@
+import { Colors, EmbedBuilder } from "discord.js";
 import { Command } from "../../modules";
 
 export default new Command({
-  name: "",
-  description: "",
+  name: "ping",
+  description: "Test the bot response time",
   run: async ({ client, interaction }) => {
+    interaction.deferReply();
 
+    const embed = new EmbedBuilder()
+      .setTitle("🏓 Pong!")
+      .setDescription(`WebSocket Ratency: ${client.ws.ping}`)
+      .setColor(Colors.Aqua)
+      .setFooter({
+        text: client.getUserData().footer,
+        iconURL: client.getUserData().icon,
+      })
+    
+    interaction.followUp({
+      embeds: [
+        embed,
+      ]
+    })
   }
 })
