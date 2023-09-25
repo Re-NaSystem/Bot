c93cf4a2afaacd63760fc2296409f197994eec4c
RenaOfficial <y.games@outlook.jp>
2023-09-25 19:19:41 +0900

    タイプミスを修正

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -9,7 +9,7 @@ export default new Command({
     await interaction.deferReply();
 
     const data = await model.findOne({ GuildID: interaction.guild?.id });
-    if (data) i18n.setLocale(data.GuildID as string);
+    if (data) client.i18n.setLocale(data.GuildID as string);
 
     await interaction.followUp({
       embeds: [
