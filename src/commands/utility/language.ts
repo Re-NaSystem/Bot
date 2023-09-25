3d009f6e3af8dcdc07e00b6a68bbf1ba9528816a
RenaOfficial <y.games@outlook.jp>
2023-09-25 18:30:49 +0900

    言語変更コマンドを作成

---
+++ src/commands/utility/language.ts
@@ -0,0 +1,53 @@
+import { Command } from '../../modules';
+import model from '../../models/language';
+import { ApplicationCommandOptionType, Colors, EmbedBuilder } from 'discord.js';
+
+export default new Command({
+  name: 'language',
+  description: 'Change the language of the Bot used on the server.',
+  options: [
+    {
+      name: 'language',
+      description: 'Language to be set.',
+      type: ApplicationCommandOptionType.String,
+      choices: [
+        {
+          name: '日本語(Japanese)',
+          value: 'ja_jp',
+        },
+        {
+          name: 'English(US)',
+          value: 'en_us',
+        }
+      ],
+      required: true
+    },
+  ],
+  run: async ({ client, interaction }) => {
+    interaction.deferReply()
+
+    const language = interaction.options.getString("language") as "ja_jp" | "en_us"
+
+    await model.findOneAndDelete({ GuildID: interaction.guild?.id })
+
+    await model.create({
+      GuildID: interaction.guild?.id,
+      Language: language
+    })
+
+    await client.i18n.setLocale(language)
+
+    interaction.followUp({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle(i18n.__('command.language.title'))
+          .setDescription(i18n.__('command.language.description'))
+          .setColor(Colors.Aqua)
+          .setFooter({
+            text: client.getUserData().footer,
+            iconURL: client.getUserData().icon,
+          })
+      ]
+    })
+  },
+});
--- test.js
+++
@@ -1,6 +0,0 @@
-const { I18n } = require("i18n");
-
-
-// i18n.setLocale('en_us')
-
-console.log(i18n.__("hello"))
